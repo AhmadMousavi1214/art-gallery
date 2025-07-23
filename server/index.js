c5127510d7ca3084af758f296ffa6299dc81029e
AhmadMousavi1214 <ahmad1414141412@gmail.com>
2025-07-24 00:46:37 +0330

    the first infrastruc added to project

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
-require('dotenv').config();
-const fastify = require('fastify')({logger : true});
-
+require("dotenv").config();
+const fastify = require("fastify")({ logger: true });
 
 const start = async () => {
-    try {
-        await fastify.listen({port : process.env.PORT})
-    } catch (err) {
-        console.log(`the error is ${err}`)
-    }
-}
+  try {
+    await fastify.listen({ port: process.env.PORT });
+    
+    console.log(`server is running correctly at the ${process.env.PORT}`);
+  } catch (err) {
+    console.log(`the error is ${err}`);
+  }
+};
+
+start();
